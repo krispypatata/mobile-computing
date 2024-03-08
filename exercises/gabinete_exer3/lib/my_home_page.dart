58bc08cd975efb5f9249c13d4570ae05b155c8ac
krispypatata <keithginoel@gmail.com>
2024-03-08 22:06:45 +0800

    updated exer03

--- exercises/gabinete_exer3/lib/my_home_page.dart
+++ exercises/gabinete_exer3/lib/my_home_page.dart
@@ -8,20 +8,30 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-  void _incrementCounter() {
-    setState(() {
-      _counter++;
-    });
-  }
+  // track the number of images/posts in the grid
+  int _imagesCounter = 0;
 
-  Widget myGrid() {
+  Widget myGrid(int imageCounter) {
     List<String> imagePaths = [
       "assets/01.jpg",
       "assets/02.jpeg",
       "assets/03.png",
-      "assets/04.jpg"
+      "assets/04.jpg",
+      "assets/05.jpg",
+      "assets/06.jpg",
+      "assets/07.jpg",
+      "assets/08.jpg",
+      "assets/09.jpg",
+      "assets/10.jpg",
+      "assets/11.jpg",
+      "assets/12.jpg",
     ];
+
+    // update the value of the imageCounter (both in the code and in the UI)
+    _imagesCounter = imagePaths.length;
+    setState(() {
+      _imagesCounter = imagePaths.length;
+    });
 
     return GridView.builder(
       gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
@@ -30,12 +40,13 @@ class _MyHomePageState extends State<MyHomePage> {
       itemCount: imagePaths.length,
       itemBuilder: (BuildContext context, int index) {
         return Padding(
-          padding: const EdgeInsets.all(3.0), // Add padding to each card
+          // add padding to each card
+          padding: const EdgeInsets.all(3.0),
           child: AspectRatio(
-            aspectRatio: 1, // Adjust this ratio as per your requirement
+            aspectRatio: 1,
             child: ClipRect(
               child: Image.asset(
-                imagePaths[index], // Path to the image
+                imagePaths[index],
                 fit: BoxFit.cover,
               ),
             ),
@@ -48,156 +59,210 @@ class _MyHomePageState extends State<MyHomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: Colors.indigo[100],
+      // *******************************************************************************************************************
+      // *******************************************************************************************************************
       appBar: PreferredSize(
-        preferredSize: Size.fromHeight(80.0), // set preferred height
+        preferredSize: const Size.fromHeight(80.0), // set preferred height
         child: AppBar(
           title: Container(
             alignment: Alignment.centerLeft,
-            padding: EdgeInsets.only(top: 30.0), // adjust top padding
-            margin: EdgeInsets.only(left: 16.0), // adjust the left margin
+            padding: const EdgeInsets.only(top: 30.0), // adjust top padding
+            margin: const EdgeInsets.only(left: 16.0), // adjust the left margin
             child: Text(
               widget.title,
-              style: TextStyle(
+              style: const TextStyle(
                 color: Colors.white,
                 fontSize: 24, // cncrease font size
                 fontWeight: FontWeight.bold, // set text to bold
               ),
             ),
           ),
-          backgroundColor: Colors.blue,
+          backgroundColor: Colors.black,
           centerTitle: true, // Center the title vertically
           titleSpacing: 0, // Set titleSpacing to 0
         ),
       ),
 
+      // *******************************************************************************************************************
+      // *******************************************************************************************************************
       // body
       body: Column(
         // ROOT NODE
-
         mainAxisAlignment: MainAxisAlignment.center,
-
         children: <Widget>[
+          // ===============================================================================================================
+          // ===============================================================================================================
           // child 1 (profile header)
-          Expanded(
+          Container(
+            padding: const EdgeInsets.only(top: 10, bottom: 30),
             child: Row(
-              // mainAxisAlignment: MainAxisAlignment.center,
+              // mainAxisAlignment: MainAxisAlignment.start,
               children: [
-                Transform.translate(
-                  offset:
-                      Offset(50, -70), // Adjust the vertical offset as needed
-                  child: ClipOval(
-                    child: Image.asset(
-                      'assets/avatar.jpg', // Provide the path to your image asset
-                      width: 100, // Adjust the width as needed
-                      height: 100, // Adjust the height as needed
-                      fit: BoxFit
-                          .cover, // Ensure the entire image is visible within the circular clipping
+                // ---------------------------------------------------------------------------------------------------------
+                // avatar (image)
+                Flexible(
+                  flex: 1,
+                  child: Padding(
+                    padding: const EdgeInsets.only(left: 20, top: 20),
+                    child: Container(
+                      decoration: BoxDecoration(
+                        shape: BoxShape.circle,
+                        border: Border.all(
+                          color: Colors.black, // Set border color
+                          width: 2.0, // Set border width
+                        ),
+                      ),
+                      child: ClipOval(
+                        child: Image.asset(
+                          'assets/avatar.jpg',
+                          width: 100,
+                          height: 100,
+                          fit: BoxFit.cover,
+                        ),
+                      ),
                     ),
                   ),
                 ),
-                Padding(
-                  padding: const EdgeInsets.only(
-                      top: 50, left: 60), // Adjust the left padding for spacing
-                  child: Column(
-                    crossAxisAlignment:
-                        CrossAxisAlignment.start, // Align the text to the start
-                    children: [
-                      Row(
-                        children: [
-                          Text(
-                            "Keith Ginoel\nGabinete",
-                            textAlign: TextAlign.left, // Align text to the left
-                          ),
-                          SizedBox(width: 30), // Add spacing between the texts
-                          Text(
-                            "Add Bio",
-                            textAlign: TextAlign.left, // Align text to the left
-                            style: TextStyle(
-                              color: Colors
-                                  .blue, // Change color to your desired color
+                // ---------------------------------------------------------------------------------------------------------
+                // BIO TEXT
+                Flexible(
+                  flex: 2,
+                  child: Container(
+                    padding: const EdgeInsets.only(top: 20, left: 10),
+                    child: Column(
+                      children: [
+                        const Row(
+                          children: [
+                            Text(
+                              "Keith Ginoel\nGabinete",
+                              textAlign:
+                                  TextAlign.left, // Align text to the left
                             ),
+                            SizedBox(
+                                width: 30), // Add spacing between the texts
+                            Text(
+                              "Add Bio",
+                              textAlign:
+                                  TextAlign.left, // Align text to the left
+                              style: TextStyle(
+                                color: Colors
+                                    .blue, // Change color to your desired color
+                              ),
+                            ),
+                          ],
+                        ),
+                        Container(
+                          padding: const EdgeInsets.only(top: 10),
+                          child: Row(
+                            children: [
+                              Text("$_imagesCounter posts"),
+                              const SizedBox(
+                                  width: 5), // add spacing between the texts
+                              const Text('|'), // divider
+                              const SizedBox(
+                                  width: 5), // add spacing between the texts
+                              const Text("10 following"),
+                              const SizedBox(
+                                  width: 5), // add spacing between the texts
+                              const Text('|'), // divider
+                              const SizedBox(
+                                  width: 5), // add spacing between the texts
+                              const Text("10 followers"),
+                            ],
                           ),
-                        ],
-                      ),
-                      Row(
-                        children: [
-                          Text("4 posts"),
-                          Text("10 following"),
-                          Text("10 followers"),
-                        ],
-                      ),
-                    ],
+                        )
+                      ],
+                    ),
                   ),
                 ),
               ],
             ),
           ),
-
-          // child 2 (Icons)
-
-          Row(
-            children: [
-              Spacer(), // Add Spacer to evenly distribute
-              // POSTS
-              Row(
-                children: [
-                  Icon(Icons.tag), // Your icon
-                  SizedBox(width: 8), // Space between icon and text
-                  Text('POSTS'), // Your text
-                ],
+          // ===============================================================================================================
+          // ===============================================================================================================
+          // simple divider
+          Theme(
+            data: ThemeData(
+              dividerTheme: DividerThemeData(
+                thickness: 1, // Change thickness as desired
+                color: Colors.grey.withOpacity(0.5), // Change opacity
               ),
-              Spacer(), // Add Spacer to evenly distribute
-              // REELS
-              Row(
-                children: [
-                  Icon(Icons.video_file), // Your icon
-                  SizedBox(width: 8), // Space between icon and text
-                  Text('REELS'), // Your text
-                ],
-              ),
-              Spacer(), // Add Spacer to evenly distribute
-              // SAVED
-              Row(
-                children: [
-                  Icon(Icons.bookmark), // Your icon
-                  SizedBox(width: 8), // Space between icon and text
-                  Text('SAVED'), // Your text
-                ],
-              ),
-              Spacer(), // Add Spacer to evenly distribute
-              // TAGGED
-              Row(
-                children: [
-                  Icon(Icons.tag_faces_rounded), // Your icon
-                  SizedBox(width: 8), // Space between icon and text
-                  Text('TAGGED'), // Your text
-                ],
-              ),
-              Spacer(), // Add Spacer to evenly distribute
-            ],
+            ),
+            child: const Divider(height: 0.1), // Set height as desired
           ),
-
+          // ===============================================================================================================
+          // ===============================================================================================================
+          // child 2 (Icons)
+          Container(
+            padding: const EdgeInsets.only(top: 20, bottom: 25),
+            child: const Row(
+              children: [
+                Spacer(), // Add Spacer to evenly distribute
+                // POSTS
+                Row(
+                  children: [
+                    Icon(Icons.tag), // Your icon
+                    SizedBox(width: 8), // Space between icon and text
+                    Text('POSTS'), // Your text
+                  ],
+                ),
+                Spacer(), // Add Spacer to evenly distribute
+                // REELS
+                Row(
+                  children: [
+                    Icon(Icons.video_file), // Your icon
+                    SizedBox(width: 8), // Space between icon and text
+                    Text('REELS'), // Your text
+                  ],
+                ),
+                Spacer(), // Add Spacer to evenly distribute
+                // SAVED
+                Row(
+                  children: [
+                    Icon(Icons.bookmark), // Your icon
+                    SizedBox(width: 8), // Space between icon and text
+                    Text('SAVED'), // Your text
+                  ],
+                ),
+                Spacer(), // Add Spacer to evenly distribute
+                // TAGGED
+                Row(
+                  children: [
+                    Icon(Icons.tag_faces_rounded), // Your icon
+                    SizedBox(width: 8), // Space between icon and text
+                    Text('TAGGED'), // Your text
+                  ],
+                ),
+                Spacer(), // Add Spacer to evenly distribute
+              ],
+            ),
+          ),
+          // ===============================================================================================================
+          // ===============================================================================================================
           // child 3 (Floating button)
-
-          // child 3 (Floating button)
-          Align(
-            alignment: Alignment.bottomLeft,
+          Container(
+            padding: const EdgeInsets.only(bottom: 10),
+            alignment: Alignment.centerLeft,
             child: Padding(
               padding: const EdgeInsets.only(left: 16.0, bottom: 16.0),
               child: FloatingActionButton(
+                shape: const CircleBorder(),
                 onPressed: () {
                   // Handle button press
                 },
                 tooltip: 'Add', // Tooltip for the button
-                backgroundColor: Colors.cyan,
-                child: Icon(Icons.add), // Icon for the button
+                backgroundColor: Colors.black,
+                child: const Icon(Icons.add,
+                    color: Colors.white), // Icon for the button
               ),
             ),
           ),
-
+          // ===============================================================================================================
+          // ===============================================================================================================
           // child 4 (Posts/Images)
           Expanded(
-            child: myGrid(),
+            child: myGrid(_imagesCounter),
           ),
         ],
       ),
